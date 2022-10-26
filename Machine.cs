2c6eaf7eb27fbde1163a6d1d3bbe8a2b04b3e288
Murilo Sertorio <alisonhenrique@msn.com>
2022-10-26 11:50:45 -0300

    Coockie

---
+++ Machine.cs
@@ -0,0 +1,65 @@
+public abstract class Machine
+{
+    public string Name { get; set; }
+    public int Coins { get; set; }
+    public int Cost { get; set; }
+} 
+
+public class Zero: Machine
+{
+        public Zero()
+    {
+        this.Name = "DevOlds";
+        this.Coins = 5;
+        this.Cost = 100;
+    }   
+}
+public class DevOlds : Machine
+{
+    public DevOlds()
+    {
+        this.Name = "DevOlds";
+        this.Coins = 5;
+        this.Cost = 100;
+    }
+}
+
+public class Xlr9 : Machine
+{
+    public Xlr9()
+    {
+        this.Name = "Xlr9";
+        this.Coins = 10;
+        this.Cost = 1000;
+    }
+}
+
+public class Xsupra : Machine
+{
+    public Xsupra()
+    {
+        this.Name = "Xsupra";
+        this.Coins = 15;
+        this.Cost = 1100;
+    }
+}
+
+public class Macha : Machine
+{
+    public Macha()
+    {
+        this.Name = "Macha";
+        this.Coins = 17;
+        this.Cost = 1250;
+    }
+}
+
+public class Zom : Machine
+{
+    public Zom()
+    {
+        this.Name = "Zom";
+        this.Coins = 20;
+        this.Cost = 1500;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,56 @@
-﻿
+﻿using System;
+
+Console.WriteLine("------- BEM VINDO AO MASTER COOCKIE CLICKER SUPRA SURF BLADE DIVERS -------");
+Console.WriteLine("clique 0 para almentar as coins:\r\n");
+bool running = true;
+int coin = 0;
+
+while (running)
+{
+    Console.WriteLine($"2 : buy                       COINS: {coin}\r\n");
+    int clicker = Convert.ToInt32(Console.ReadLine());
+    switch (clicker)
+    {
+        case 0:
+            coin = coin + 10;
+            break;
+
+        case 2:
+            Console.WriteLine("\r\n------- MENU -------\r\n");
+            Console.WriteLine("MACHINES:\r\n");
+            Console.WriteLine("1 - DevOlds    $: 100     clickers: 5\r\n2 - Xlr9       $: 1000    clickers: 10\r\n3 - DevOlds    $: 1100    clickers: 15\r\n4 - Macha      $: 1250    clickers: 17\r\n5 - DevOlds    $: 1500    clickers: 20\r\n");
+            int buy = Convert.ToInt32(Console.ReadLine());         
+            DevOlds devOlds = new DevOlds();
+            Xlr9 xlr9 = new Xlr9();
+            Xsupra xsupra = new Xsupra();
+            Macha macha = new Macha();
+            Zom zom = new Zom();
+            if (buy == 1 && coin > devOlds.Cost)
+            {
+                coin = coin - devOlds.Cost;
+            }
+            if (buy == 2 && coin > xlr9.Cost)
+            {
+                coin = coin - xlr9.Cost;
+            }
+            if (buy == 3 && coin > xsupra.Cost)
+            {
+                coin = coin - xsupra.Cost;
+            }
+            if (buy == 4 && coin > macha.Cost)
+            {
+                coin = coin - macha.Cost;
+            }
+            if (buy == 5 && coin > zom.Cost)
+            {
+                coin = coin - zom.Cost;
+            }
+            break;
+
+        case 1:
+            Console.WriteLine($"Você ficou com {coin}");
+            running = false;
+            break;
+    }
+
+}
